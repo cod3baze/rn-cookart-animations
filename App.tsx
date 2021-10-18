40f51269244e2d2fa4c60de8674cd7e6fe347d0a
elias alexandre <elliasallex@gmail.com>
2021-10-18 12:43:47 -0300

    feat: SVG Animations initial

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import "react-native-gesture-handler";
 
 import React from "react";
 import AppLoading from "expo-app-loading";
+
+import { Righteous_400Regular } from "@expo-google-fonts/righteous";
 import {
   useFonts,
   Ubuntu_400Regular,
@@ -13,6 +15,7 @@ import TindevLika from "./src/screens/TindevLika";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
+    Righteous_400Regular,
     Ubuntu_400Regular,
     Ubuntu_500Medium,
     Ubuntu_700Bold,
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,27 @@
 export const theme = {
   colors: {
-    orange: '#BF612A',
-    white: '#f7f7f7',
-    brown: '#591C27',
-    gray: '#afaca3',
-    red: '#A61B34',
+    orange: "#BF612A",
+    white: "#f7f7f7",
+    brown: "#591C27",
+    gray: "#afaca3",
+    red: "#A61B34",
+
+    gray100: "#212028",
+    gray80: "#2f2e3a",
+
+    blue100: "#0051dc",
+    blue90: "#0087dc",
+    blue70: "#32BAFA",
   },
 
   fonts: {
-    primary400: 'Ubuntu_400Regular',
-    primary500: 'Ubuntu_500Medium',
-    primary700: 'Ubuntu_700Bold',
-  }
+    primary400: "Ubuntu_400Regular",
+    primary500: "Ubuntu_500Medium",
+    primary700: "Ubuntu_700Bold",
+
+    regular: "Ubuntu_400Regular",
+    medium: "Ubuntu_500Medium",
+    bold: "Ubuntu_700Bold",
+    extraBold: "Righteous_400Regular",
+  },
 };
