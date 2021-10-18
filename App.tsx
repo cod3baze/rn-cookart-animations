5fcfe15e2993fa5b0933e44b13dc945306dfe298
elias alexandre <elliasallex@gmail.com>
2021-10-18 13:03:03 -0300

    feat: SVG Button styles

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
   Ubuntu_700Bold,
 } from "@expo-google-fonts/ubuntu";
 
-import TindevLika from "./src/screens/TindevLika";
+import { DrinkWater } from "./src/screens/DrinkWater";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,5 +25,5 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <TindevLika />;
+  return <DrinkWater />;
 }
--- src/screens/DrinkWater/index.tsx
+++ src/screens/DrinkWater/index.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
 
+import { Svg, Circle } from "react-native-svg";
+
 import { Fontisto } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 import { theme } from "../../styles/theme";
 import { Header } from "../../components/Water/Header";
 
-export function Home() {
+function DrinkWater() {
   return (
     <View style={styles.container}>
       <Header ml={0} percent={0} />
 
       <View style={styles.footer}>
         <TouchableOpacity style={styles.button}>
+          <Svg width={120} height={120}>
+            <Circle
+              cx="60"
+              cy="60"
+              r="40"
+              fill={theme.colors.blue100}
+              stroke={theme.colors.blue90}
+              strokeWidth={17}
+              strokeOpacity={0.5}
+            />
+          </Svg>
+
           <Fontisto
             name="blood-drop"
             size={32}
@@ -25,3 +39,5 @@ export function Home() {
     </View>
   );
 }
+
+export { DrinkWater };
