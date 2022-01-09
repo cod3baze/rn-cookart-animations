a1433fd813bea658336c6e4aaa820bf2637cd875
elias alexandre <elliasallex@gmail.com>
2022-01-09 19:24:43 -0300

    feat: Loading animation

--- src/screens/LoadingAnimation/index.tsx
+++ src/screens/LoadingAnimation/index.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { MotiView } from "@motify/components";
 
 import { View } from "react-native";
 
 const LoadingIndicator = ({ size }: { size: number }) => {
-  return <View />;
+  return (
+    <MotiView
+      from={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth: 0,
+        shadowOpacity: 0.5,
+      }}
+      animate={{
+        width: size + 20,
+        height: size + 20,
+        borderRadius: (size + 20) / 2,
+        // borderWidth: size / 10,
+        borderWidth: size / 10 + 2,
+        shadowOpacity: 1,
+      }}
+      transition={{
+        type: "timing",
+        duration: 1000,
+        loop: true,
+      }}
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth: size / 10,
+        borderColor: "#fff",
+        shadowColor: "#fff",
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+      }}
+    />
+  );
 };
 
 export function LoadingAnimation() {
@@ -16,7 +51,7 @@ export function LoadingAnimation() {
         backgroundColor: "#010101",
       }}
     >
-      <LoadingIndicator size={30} />
+      <LoadingIndicator size={80} />
     </View>
   );
 }
