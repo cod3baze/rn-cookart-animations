08b9a6c1c22994150c9452a5ea16623e04d369d3
elias alexandre <elliasallex@gmail.com>
2022-07-11 23:44:14 -0300

    shazam: ini styles ui

---
+++ src/screens/Shazam/styles.ts
@@ -0,0 +1,22 @@
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+type HeaderProps = {
+  screen: "Library" | "Shazam" | "Settings";
+};
+
+const { width, height } = Dimensions.get("screen");
+
+export const Container = styled.View`
+  width: ${width}px;
+  height: ${height - 30}px;
+
+  padding-top: ${getStatusBarHeight()}px;
+`;
+
+export const Header = styled.View<HeaderProps>`
+  width: 100%;
+
+  background-color: ${({ screen }) => (screen === "Shazam" ? "#f23" : "#e8e8")};
+`;
