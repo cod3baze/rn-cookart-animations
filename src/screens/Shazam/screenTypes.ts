e4f6def37e3d1696cdb4f16affdf6c551a94f0bd
elias alexandre <elliasallex@gmail.com>
2022-07-11 23:43:41 -0300

    shazam: screen data helper

---
+++ src/screens/Shazam/screenTypes.ts
@@ -0,0 +1,19 @@
+export type ScrollableScreenKeys = {
+  id: "Shazam" | "Settings" | "Library";
+  name: string;
+};
+
+export const screenTypes: ScrollableScreenKeys[] = [
+  {
+    id: "Shazam",
+    name: "Shazam",
+  },
+  {
+    id: "Settings",
+    name: "Settings",
+  },
+  {
+    id: "Library",
+    name: "Library",
+  },
+];
